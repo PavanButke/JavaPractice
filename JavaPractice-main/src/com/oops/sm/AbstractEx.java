29f7df5ae282e2400b3e6f9ff0a0dfc5029e934e
PavanButke <butkepavan55@gmail.com>
2022-08-29 17:33:39 +0530

    Abstract Done !

---
+++ JavaPractice-main/src/com/oops/sm/AbstractEx.java
@@ -0,0 +1,71 @@
+package com.oops.sm;
+
+import java.util.*;
+
+public class AbstractEx {
+
+	public static void main(String args[]) {
+		
+		
+//		CarCompany obj= new CarCompany("A",11); --> Not Allowed
+//		obj.Millege();
+		
+		CarCompany obj= new Toyota(null , "A",112);
+		obj.Millege();
+		obj= new Ferrari("hi","B",113);
+		obj.Millege();
+		
+	}
+
+	static abstract class CarCompany {
+		String carType;
+		int price;
+
+
+
+		public CarCompany(String carType2, int price2) {
+			// TODO Auto-generated constructor stub
+		}
+
+		void CarCompany(String carType, int type) {
+
+			this.carType = carType;
+			this.price = price;
+		}
+
+		void Millege() {
+				System.out.println("Car is of Type "+carType);
+		}
+
+	}
+
+	static class Toyota extends CarCompany {
+		List<Integer> productionCap;
+
+		Toyota(List<Integer> productionCap , String carType , int price) {
+			super(carType , price);
+			this.productionCap= productionCap;
+		}
+
+		void Millege() {
+			System.out.println("Car is of Type "+carType);
+		}
+
+	}
+
+	static  class Ferrari extends CarCompany {
+
+		String highDemand;
+		
+		Ferrari(String highDemand, String carType , int price){
+		
+			super(carType , price);
+			this.highDemand= highDemand;
+		}
+			
+		void Millege() {
+			System.out.println("Car's price is "+price);
+		}
+
+	}
+}
